32c4bf6f5330394edd33ca52f3e1a9fb2ec3899b
kalidindi-naveen <step.into.iot@gmail.com>
2024-02-07 15:47:14 +0530

    Added TF Files

--- SG.tf
+++ SG.tf
@@ -40,6 +40,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+  ingress {
+    description      = "Open 8083"
+    from_port        = 8083
+    to_port          = 8083
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
